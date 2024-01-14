1567623a0589b6f08af74e4b60d63e2c67f9e29c
hongchang <hongchangpz@gmail.com>
2024-01-13 20:07:14 -0800

    写光标位置

--- oskernel/init/main.c
+++ oskernel/init/main.c
@@ -8,17 +8,16 @@ void kernel_main(void) {
     char* s = "linux";
     printk("%s\n", s);
 
-    uchar cursor_position_high = 0;
-    uchar cursor_position_low = 0;
 
-    out_byte(0x3D4, 0x0e);
-    cursor_position_high = in_byte(0x3D5);
+    ushort cursor = 80 * 1 + 40;
+    uchar low = cursor & 0xff;
+    uchar height = cursor >> 8;
 
-    out_byte(0x3D4, 0x0f);
-    cursor_position_low = in_byte(0x3D5);
+    out_byte(0x3d4, 0x0e);
+    out_byte(0x3d5, height);
 
-    ushort position = cursor_position_high << 8 | cursor_position_low;
-    printk("%d, %d, %d\n", cursor_position_high, cursor_position_low, position);
+    out_byte(0x3d4, 0x0f);
+    out_byte(0x3d5, low);
 
     while (true);
 }
