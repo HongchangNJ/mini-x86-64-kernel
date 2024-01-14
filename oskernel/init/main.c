0e30a8d194044c6717ee1228722fc9c048553607
hongchang <hongchangpz@gmail.com>
2024-01-13 19:52:28 -0800

    读光标位置

--- oskernel/init/main.c
+++ oskernel/init/main.c
@@ -1,6 +1,3 @@
-//
-// Created by ziya on 22-6-23.
-//
 
 #include "../include/linux/tty.h"
 #include "../include/linux/kernel.h"
@@ -8,12 +5,20 @@
 
 void kernel_main(void) {
     console_init();
+    char* s = "linux";
+    printk("%s\n", s);
 
-    char* s = "learn linux";
+    uchar cursor_position_high = 0;
+    uchar cursor_position_low = 0;
 
-    printk("name: %s\n", s);
+    out_byte(0x3D4, 0x0e);
+    cursor_position_high = in_byte(0x3D5);
 
+    out_byte(0x3D4, 0x0f);
+    cursor_position_low = in_byte(0x3D5);
 
+    ushort position = cursor_position_high << 8 | cursor_position_low;
+    printk("%d, %d, %d\n", cursor_position_high, cursor_position_low, position);
 
     while (true);
 }
